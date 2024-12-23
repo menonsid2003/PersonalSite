5cb1de37933b2da7ce0e33dd76c095851bd9b37e
menonsid2003 <menonsid2003@gmail.com>
2024-12-23 01:37:55 -0500

    build prep line fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import styles from './App.module.scss';
 import Card from './components/carousel';
 //import MusicPlayer from './components/musicplayer';
