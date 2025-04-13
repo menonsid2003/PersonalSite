a2317f3fc5647ab06caf50ceb37caf7f41048902
Sidharth Menon <menonsid2003@gmail.com>
2025-04-13 12:00:37 -0400

    Update contact.tsx
    added github link and updated resume link.
    edited directly in github lol we'll see if it actually updates properly in prod

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,13 +6,14 @@ const Contact: React.FC = () => {
         <div className = {styles.contacts}>
             <h2>Contact</h2>
             <ul>
-                Feel free to reach out to me via:
+                Feel free to reach out to me or check me out via:
                 <li> <img src='https://upload.wikimedia.org/wikipedia/commons/8/8c/Gmail_Icon_%282013-2020%29.svg'></img> : <a href="menonsid2003@gmail.com">menonsid2003@gmail.com</a></li>
                 <li> <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"></img> : <a href="https://www.linkedin.com/in/menonsid2003" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/menonsid2003</a></li>
+                <li> <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/960px-Octicons-mark-github.svg.png'></img> : <a href="https://github.com/menonsid2003" target="_blank" rel="noopener noreferrer">https://github.com/menonsid2003</a></li>
             </ul>
             <p><br></br></p>
             <h2> Latest Resume</h2>
-            <iframe src="https://personalportfoliopublicassets.s3.us-east-1.amazonaws.com/dec_resume.pdf"></iframe>
+            <iframe src="https://personalportfoliopublicassets.s3.us-east-1.amazonaws.com/SidharthMenonLatestResume.pdf"></iframe>
         </div>
     );
 }
